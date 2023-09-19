9d863124295a63c17b618e6ca65d041d31e78922
Brandon Begle <brandon@begleweyer.com>
2023-09-19 09:01:26 -0400

    Change notification check type to cached instead of time period check

--- src/Console/Commands/SlowQueueCheck.php
+++ src/Console/Commands/SlowQueueCheck.php
@@ -42,14 +42,16 @@ class SlowQueueCheck extends Command
 
             $seconds = $this->argument('seconds');
             $greaterThanPeriod = now()->diffInSeconds($time) > $seconds;
-            $notGreaterThanTwoPeriods = now()->diffInSeconds($time) < ($seconds * 2);
+            $notificationCache = Cache::get('laravel-queue-monitoring:slow-queue-notification-sent');
 
-            if($greaterThanPeriod && $notGreaterThanTwoPeriods){
+            if($greaterThanPeriod && !$notificationCache){
                 $this->info('Dispatching Event');
+                Cache::put('laravel-queue-monitoring:slow-queue-notification-sent', true);
                 event(new SlowQueueEvent($this->argument('seconds')));
             }
         }else{
             Cache::put('laravel-queue-monitoring:slow-queue-check', now()->timestamp);
+            Cache::put('laravel-queue-monitoring:slow-queue-notification-sent', false);
             dispatch(new SlowQueryCheckJob());
         }
 
